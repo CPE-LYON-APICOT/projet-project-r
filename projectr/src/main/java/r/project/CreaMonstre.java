b79c4b260521300bd1245a01f49120df862db5f5
Caenau <Thomas.bourcet@gmail.com>
2024-04-19 11:20:48 +0200

    creaMonstreIni

--- projectr/src/main/java/r/project/CreaMonstre.java
+++ projectr/src/main/java/r/project/CreaMonstre.java
@@ -1,6 +1,7 @@
 package r.project;
 
 public class CreaMonstre extends Creature {
+    
     public CreaMonstre(int pPv, int pAttaque,String pNom, String pDescription, String pLienImage, Faction pFaction) {
         super(pPv, pAttaque,pNom, pDescription, pLienImage, pFaction);
     }
