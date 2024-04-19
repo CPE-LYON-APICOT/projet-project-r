9165e5251d934d0590b83f632e76a9c5a22e6de8
Tita <teo.languebien@orange.fr>
2024-04-19 16:58:21 +0200

    affichage et ajout de game controller

--- projectr/src/main/java/r/project/GameController.java
+++ projectr/src/main/java/r/project/GameController.java
@@ -103,30 +103,30 @@ public class GameController {
         Image image = new Image(getClass().getResourceAsStream(cartesList.get(0).getLienImage()));
         card1Image.setImage(image);
         
-        card1LabelNom.setText("Nom : "+cartesList.get(0).getNom());
-        card1LabelAttaque.setText("Attack : "+String.valueOf(lstMonster.get(0).getAttaque()));
-        card1LabelPV.setText("Pv : "+String.valueOf(lstMonster.get(0).getPv()));
+        card1LabelNom.setText(cartesList.get(0).getNom());
+        card1LabelAttaque.setText(String.valueOf(lstMonster.get(0).getAttaque()));
+        card1LabelPV.setText(String.valueOf(lstMonster.get(0).getPv()));
         Image image2 = new Image(getClass().getResourceAsStream(cartesList.get(1).getLienImage()));
         card2Image.setImage(image2);
-        card2LabelNom.setText("Nom : "+cartesList.get(1).getNom());
-        card2LabelAttaque.setText("Attack : "+String.valueOf(lstMonster.get(1).getAttaque()));
-        card2LabelPV.setText("Pv : "+String.valueOf(lstMonster.get(1).getPv()));
+        card2LabelNom.setText(cartesList.get(1).getNom());
+        card2LabelAttaque.setText(String.valueOf(lstMonster.get(1).getAttaque()));
+        card2LabelPV.setText(String.valueOf(lstMonster.get(1).getPv()));
         Image image3 = new Image(getClass().getResourceAsStream(cartesList.get(2).getLienImage()));
         card3Image.setImage(image3);
-        card3LabelNom.setText("Nom : "+cartesList.get(2).getNom());
-        card3LabelAttaque.setText("Attack : "+String.valueOf(lstMonster.get(2).getAttaque()));
-        card3LabelPV.setText("Pv : "+String.valueOf(lstMonster.get(2).getPv()));
+        card3LabelNom.setText(cartesList.get(2).getNom());
+        card3LabelAttaque.setText(String.valueOf(lstMonster.get(2).getAttaque()));
+        card3LabelPV.setText(String.valueOf(lstMonster.get(2).getPv()));
 
         Image image4 = new Image(getClass().getResourceAsStream(cartesList.get(3).getLienImage()));
         card4Image.setImage(image4);
-        card4LabelNom.setText("Nom : "+cartesList.get(3).getNom());
-        card4LabelAttaque.setText("Attack : "+String.valueOf(lstMonster.get(3).getAttaque()));
-        card4LabelPV.setText("Pv : "+String.valueOf(lstMonster.get(3).getPv()));
+        card4LabelNom.setText(cartesList.get(3).getNom());
+        card4LabelAttaque.setText(String.valueOf(lstMonster.get(3).getAttaque()));
+        card4LabelPV.setText(String.valueOf(lstMonster.get(3).getPv()));
         Image image5 = new Image(getClass().getResourceAsStream(cartesList.get(4).getLienImage()));
         card5Image.setImage(image5);
-        card5LabelNom.setText("Nom : "+cartesList.get(4).getNom());
-        card5LabelAttaque.setText("Attack : "+String.valueOf(lstMonster.get(4).getAttaque()));
-        card5LabelPV.setText("Pv : "+String.valueOf(lstMonster.get(4).getPv()));
+        card5LabelNom.setText(cartesList.get(4).getNom());
+        card5LabelAttaque.setText(String.valueOf(lstMonster.get(4).getAttaque()));
+        card5LabelPV.setText(String.valueOf(lstMonster.get(4).getPv()));
 
     }
 
